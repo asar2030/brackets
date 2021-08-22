b17c8312514ab3e75f8d23bf1c3071380b807ca4
Asar Akhmetbekov <asarakhmetbekov@MacBook-Pro-Asar.local>
2021-08-22 20:49:57 +0600

    feat: try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 > 0) return false
+  let res = []
+  for (let i = 0; i < str.length; i++) {
+      for (let bracket of bracketsConfig) {
+          if (str[i] === bracket[0] && str[i + 1] === bracket[1]) {
+              res = str.replace(bracketsConfig[0]+bracketsConfig[1], '')
+              return true 
+          }
+
+      }
+  }
+  if (res.length % 2 == 0) return false
+  else return true
 }
